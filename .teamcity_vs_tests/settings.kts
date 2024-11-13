d5ad29e4eeafd3e3841abcb1b37cb787a8cfe0fd
dkrupkina <dkrupkina@TeamCity>
2024-11-13 16:41:52 +0000

    TeamCity change in 'VisualStudioTest: XUnit Dotnet6' project: Versioned settings configuration updated

---
+++ .teamcity_vs_tests/settings.kts
@@ -0,0 +1,70 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
+import jetbrains.buildServer.configs.kotlin.buildSteps.vstest
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2024.03"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "✅ Build"
+
+    params {
+        param("teamcity.dotnet.mstest.16.0", """C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\MSTest.exe""")
+        param("teamcity.dotnet.vstest.16.0", """C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\Extensions\TestPlatform\vstest.console.exe""")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetBuild {
+            id = "dotnet"
+            projects = "MSTestCore.sln"
+            sdk = "6"
+        }
+        vstest {
+            id = "VisualStudioTest"
+            vstestPath = "%teamcity.dotnet.vstest.16.0%"
+            includeTestFileNames = """C:\TeamCityAgent_24_03\work\2a90eab428af608f\PrimeService.Tests\bin\Debug\net6.0\PrimeService.Tests.dll"""
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
